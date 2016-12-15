886e2e564e09c9ce384112216e96211f8e4f3966
chenlei <chenlei@likingfit.com>
2016-12-15 20:58:55 +0800

    简述：添加更新界面UI
    背景信息：
    
    修改内容：

---
+++ app/src/main/java/com/liking/treadmill/activity/UpdateActivity.java
@@ -0,0 +1,35 @@
+package com.liking.treadmill.activity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.aaron.android.framework.utils.DisplayUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * 说明:
+ * Author: chenlei
+ * Time: 下午5:51
+ */
+
+public class UpdateActivity extends LikingTreadmillBaseActivity{
+
+//    @BindView(R.id.update_layout)
+//    FrameLayout mUpdateLayout;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.layout_update);
+//        ButterKnife.bind(this);
+//        initView();
+//    }
+//
+//    public void initView() {
+//        int height = DisplayUtils.getHeightPixels();
+//        int mProgressHeight = height / 2;
+//        mUpdateLayout.getLayoutParams().width = mProgressHeight;
+//        mUpdateLayout.getLayoutParams().height = mProgressHeight;
+//    }
+}
