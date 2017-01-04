e4598e5e2c82de4517b8356aa4b6efc50daf8ec1
chenlei <chenlei@likingfit.com>
2017-01-04 10:52:21 +0800

    简述：添加解绑消息处理
    背景信息：
    
    修改内容：

--- app/src/androidTest/java/com/liking/treadmill/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/liking/treadmill/ExampleInstrumentedTest.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.google.gson.Gson;
+import com.liking.treadmill.socket.result.MemberListResult;
+import com.liking.treadmill.storge.Preference;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +26,22 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.liking.treadmill", appContext.getPackageName());
+    }
+
+    @Test
+    public void getMemberList() throws Exception {
+        String memberlist = "{type:\"member_list\",msg_id:0,data:{\"bracelet_id\":{\"member\":[12345,54321],\"manger\":[12345,54321]}}}";
+        Gson gson = new Gson();
+        MemberListResult memberListResult = gson.fromJson(memberlist, MemberListResult.class);
+        MemberListResult.MemberData memberData = memberListResult.getData();
+        if (memberData != null) {
+            MemberListResult.MemberData.MemberListData memberList = memberData.getBraceletId();
+            try {
+                Preference.setMemberList(gson.toJson(memberList));
+            }catch (Exception e) {
+            }
+        }
+        MemberListResult.MemberData members = gson.fromJson(Preference.getMemberList(), MemberListResult.MemberData.class);
+        System.out.println();
     }
 }
