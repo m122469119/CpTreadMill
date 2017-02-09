559061c7417a83d59f6f3d07d82c122647c3a8dd
chenlei <chenlei@likingfit.com>
2017-02-09 15:25:57 +0800

    简述：添加广告图下载
    背景信息：
    
    修改内容：

--- app/src/main/java/com/liking/treadmill/mvp/presenter/AdvertisementPresenter.java
+++ app/src/main/java/com/liking/treadmill/mvp/presenter/AdvertisementPresenter.java
@@ -2,11 +2,16 @@ package com.liking.treadmill.mvp.presenter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 
 import com.aaron.android.codelibrary.utils.DateUtils;
+import com.aaron.android.codelibrary.utils.FileUtils;
 import com.aaron.android.codelibrary.utils.LogUtils;
+import com.aaron.android.codelibrary.utils.StringUtils;
+import com.aaron.android.framework.base.BaseApplication;
 import com.aaron.android.framework.library.downloadprovider.FileDownloadService;
 import com.aaron.android.framework.library.storage.DiskStorageManager;
+import com.google.gson.Gson;
 import com.liking.treadmill.message.AdvertisementMessage;
 import com.liking.treadmill.receiver.AdvertisementReceiver;
 import com.liking.treadmill.service.ApkDownloadService;
@@ -15,6 +20,7 @@ import com.liking.treadmill.socket.result.AdvertisementResult;
 import com.liking.treadmill.storge.Preference;
 import com.liking.treadmill.utils.AlarmManagerUtils;
 
+import java.io.File;
 import java.util.List;
 
 import de.greenrobot.event.EventBus;
@@ -32,22 +38,80 @@ public class AdvertisementPresenter {
     /**
      * 下载广告资源
      */
-    public void advertisementResDownload(Context context, String advUrl) {
+    public void advResDownload(Context context, String advUrl) {
         Intent intent = new Intent(context, FileDownloadService.class);
-        intent.putExtra(ApkDownloadService.EXTRA_DOWNLOAD_URL, advUrl);
-        intent.putExtra(ApkDownloadService.EXTRA_DOWNLOAD_PATH, advPath);
+        intent.putExtra(FileDownloadService.EXTRA_DOWNLOAD_URL, advUrl);
+        intent.putExtra(FileDownloadService.EXTRA_DOWNLOAD_PATH, advPath);
         context.startService(intent);
     }
 
-    public boolean loadAdvertisementlocalRes() {
+    public boolean loadAdvlocalRes() {
+
         return false;
     }
 
-    public boolean loadAdvertisementRemoteRes() {
+    public boolean loadAdvRemoteRes() {
         return false;
     }
 
 
-    public void loadAdvertisementRes(Context context, List<AdvertisementResult.AdvUrlResource.Resource> resources) {
+    public void showAdvRes(Context context, boolean isCacheData, String res) {
+        Gson gson = new Gson();
+
+        try {
+            AdvertisementResult advertisementResult = gson.fromJson(res, AdvertisementResult.class);
+            AdvertisementResult.AdvUrlResource advUrlResource = advertisementResult.getUrlResources();
+            if(advUrlResource != null) {
+                int requestcode = 1000;
+                long serviceTime = DateUtils.currentDataSeconds() + SocketHelper.sTimestampOffset;
+                List<AdvertisementResult.AdvUrlResource.Resource> reslist = advUrlResource.getResources();
+                LogUtils.d("aaron", "request: " + reslist.size());
+                for (AdvertisementResult.AdvUrlResource.Resource resource: reslist) {
+                    LogUtils.d("aaron", "request: " + resource.getUrl());
+                    long advTime = DateUtils.parseString("yyyyMMdd", resource.getEndtime()).getTime();
+                    long timeOffset = advTime - serviceTime * 1000;
+
+                    Intent intent = new Intent(context, AdvertisementReceiver.class);
+                    intent.putExtra(AdvertisementReceiver.ADVERTISEMENT_URL, resource.getUrl());
+                    intent.putExtra(AdvertisementReceiver.ADVERTISEMENT_ENDTIME, resource.getEndtime());
+                    intent.putExtra(AlarmManagerUtils.REQUESTCODE, requestcode);
+
+                    if(timeOffset > 0) {
+                        if(!isCacheData) {
+                            String downPath = getDownloadFilePath(advPath, resource.getUrl());
+                            resource.setLocalPath(downPath);
+                            advResDownload(context,resource.getUrl());
+                        }
+                        AlarmManagerUtils.addAdvertisementAlarm(context.getApplicationContext(), intent, timeOffset);
+                    } else {
+                        resource.setOpen(false);
+                        AlarmManagerUtils.removeAdvertisementAlarm(context.getApplicationContext(), intent);
+                    }
+                    requestcode = requestcode + 1;
+                }
+                //save Preference
+                Preference.setAdvertisementResource(gson.toJson(advertisementResult));
+                EventBus.getDefault().post(new AdvertisementMessage(reslist));
+            }
+        }catch (Exception e) {}
+    }
+
+    private String getDownloadFilePath(String directoryPath, String url) {
+        if (StringUtils.isEmpty(directoryPath)) {
+            directoryPath = FileUtils.createFolder(getDiskCacheDir()).getPath();
+        }
+        return directoryPath + File.separator + FileUtils.getFileName(url);
+    }
+
+    private String getDiskCacheDir() {
+        String cachePath;
+        Context context = BaseApplication.getInstance();
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable()) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return cachePath;
     }
 }
