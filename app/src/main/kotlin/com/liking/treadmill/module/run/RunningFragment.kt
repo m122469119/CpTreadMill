6ef5ca64474baa55f801f09740a43aa3b251bae3
chenlei <chenlei@likingfit.com>
2017-08-16 17:45:47 +0800

    简述：添加跑步页面
    背景信息：
    
    修改内容：

---
+++ app/src/main/kotlin/com/liking/treadmill/module/run/RunningFragment.kt
@@ -0,0 +1,17 @@
+package com.liking.treadmill.module.run
+
+import com.liking.treadmill.fragment.base.SerialPortFragment
+
+/**
+ * Created on 2017/08/16
+ * desc:
+
+ * @author: chenlei
+ * *
+ * @version:1.0
+ */
+
+class RunningFragment : SerialPortFragment() {
+
+
+}
